2989cac9ac3f671e4b21f6a2ad698ed058c2168d
anish9999 <anishshah9876@gmail.com>
2023-01-28 00:12:14 -0800

    updation is half way

--- base/models.py
+++ base/models.py
@@ -8,3 +8,11 @@ class BusStopLoc(models.Model):
     lon = models.FloatField()
     location = models.CharField(max_length = 100)
     created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+class BusCapacityCount(models.Model):
+    End_Time =models.DateTimeField(auto_now_add=True)
+    In = models.IntegerField()
+    Out = models.IntegerField()
+    Total_Inside = models.IntegerField() 
+    
